ff781234a47a3180c027ef1a958b1a9beae20b58
Brylie Christopher Oxley <brylie@creativecommons.org>
2022-01-11 10:32:33 +0200

    Format with Prettier

--- webpack.config.js
+++ webpack.config.js
@@ -1,40 +1,42 @@
-const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin')
-const CopyPlugin = require('copy-webpack-plugin')
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/index.js',
+  entry: "./src/index.js",
   output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'js/vocabulary-styles.js',
-    library: 'vocabulary_styles'
+    path: path.resolve(__dirname, "dist"),
+    filename: "js/vocabulary-styles.js",
+    library: "vocabulary_styles",
   },
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [
-        MiniCssExtractPlugin.loader,
-        {
-          loader: 'css-loader'
-        },
-        {
-          loader: 'sass-loader',
-          options: {
-            sourceMap: true
-          }
-        }
-      ]
-    }]
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: "css-loader",
+          },
+          {
+            loader: "sass-loader",
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
-      filename: 'css/vocabulary-styles.css'
+      filename: "css/vocabulary-styles.css",
     }),
     new CopyPlugin({
       patterns: [
-        { from: 'src/styles', to: 'scss' },
-        { from: 'src/assets', to: 'assets' }
-      ]
-    })
-  ]
-}
+        { from: "src/styles", to: "scss" },
+        { from: "src/assets", to: "assets" },
+      ],
+    }),
+  ],
+};
